da2b229f86c2bb8e20d62ff33659701826440e37
Ray Yip <raymondyip0903@gmail.com>
2021-04-01 10:54:40 +1100

    added histograms

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(shinydashboard)
 library(dplyr)
-
+library(org.Mm.eg.db)
 # Define UI for data upload app ----
 ui <- dashboardPage(
   
@@ -23,9 +23,14 @@ ui <- dashboardPage(
   
   # Main panel for displaying outputs ----
   dashboardBody(
-    plotOutput('contents'),
+    
     # Output: Data file ----
-    tableOutput("contents")
+    tableOutput("contents"),
+    downloadButton("dl_table", "Download your table here!"),
+    
+    # Output: Boxplot ----
+    plotOutput("boxplot_fc"), 
+    plotOutput("histogram_pvalue")
     
   )
   
@@ -38,7 +43,7 @@ server <- function(input, output) {
   mydata <- reactiveValues()
   
   # The following code runs when a file is uploaded
-  # We want to load data, and annotate it with out databases here
+  # We want to load data, and annotate it with our databases here
   # We first load the data based on its extension (.csv, .txt, .xlsx)
   # Then we load our databases and join the user data with out provided databases
   # Finally we save the df as a reactive object
@@ -77,8 +82,11 @@ server <- function(input, output) {
       )
     }
     
-    
-    
+    entrezids <- as.list(org.Mm.egALIAS2EG)
+    names(df)[1] <- "ID"
+    df <- cp_idconvert(df, cp_idtype(df$ID))
+    #insert Jeff's functino (cp_idconverter)
+      #changing uniprot id to gene names
     #provided_data <- read.csv(file='database/knocktf.csv')
     
     #df <- left_join(df, provided_data, by = c('genenames'='genenames'))
@@ -86,22 +94,58 @@ server <- function(input, output) {
      
     
     
-    provided_data <- read.csv(file='database/knocktf.csv')
-    df <- left_join(df, provided_data, by = c('genenames'='genenames'))
+    #provided_data <- read.csv(file='knocktf/differential expression of genes in all datasets.txt')
+    #df <- left_join(df, provided_data, by = c('ID'='genenames'))
+  
     mydata$protdf <- df 
+    print(df)
   })
+  
   
   
   
   # The table we make can use our reactive protdf dataframe
   output$contents <- renderTable({
-    boxplot(
-    req(mydata$protdf),
+
+    req(mydata$protdf)
     
-    return(head(mydata$protdf)) )
+    return(head(mydata$protdf)) 
     #Q=why do we need this line of command
     
   })
+  # The table we make can use our reactive protdf dataframe
+  output$contents <- renderTable({
+    
+    req(mydata$protdf)
+    
+    return(head(mydata$protdf)) 
+    #Q=why do we need this line of command
+    
+  })
+  
+  # Render a histogram of pvalues
+  output$histogram_pvalue <- renderPlot({
+    
+    req(mydata$protdf)
+    
+    x1 <<- mydata$protdf
+    
+    print(head(mydata$protdf))
+    
+    return(hist(mydata$protdf[,3]))
+  })
+  
+  # Render a boxplot of fold-change
+  output$boxplot_fc <- renderPlot({
+    
+    req(mydata$protdf)
+    
+    return(boxplot(mydata$protdf[,4]))    
+  })
+  
+  # Downloader for the table
+  output$dl_table <- cp_dl_table_csv(mydata$protdf, "annotated_data.csv")
+  
   
 }
 
